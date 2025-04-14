cb0d3ad4b9739191068ef3d24ade28e0fb69f949
Tomasz Tybulewicz <tomasz@tybulewicz.com>
2025-04-14 09:02:03 +0200

    CVS-164032 update license in interactive ai (#14)

--- interactive_ai/libs/sc_sdk/sc_sdk/adapters/tensor_adapter.py
+++ interactive_ai/libs/sc_sdk/sc_sdk/adapters/tensor_adapter.py
@@ -2,19 +2,8 @@
 
 import numpy as np
 
-# INTEL CONFIDENTIAL
-#
-# Copyright (C) 2021 Intel Corporation
-#
-# This software and the related documents are Intel copyrighted materials, and
-# your use of them is governed by the express license under which they were provided to
-# you ("License"). Unless the License provides otherwise, you may not use, modify, copy,
-# publish, distribute, disclose or transmit this software or the related documents
-# without Intel's prior written permission.
-#
-# This software and the related documents are provided as is,
-# with no express or implied warranties, other than those that are expressly stated
-# in the License.
+# Copyright (C) 2022-2025 Intel Corporation
+# LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 from sc_sdk.adapters.binary_interpreters import NumpyBinaryInterpreter
 from sc_sdk.entities.interfaces.tensor_adapter_interface import TensorAdapterInterface
 from sc_sdk.repos.storage.binary_repos import TensorBinaryRepo
--- interactive_ai/libs/sc_sdk/sc_sdk/configuration/utils/parameter_class_helper.py
+++ interactive_ai/libs/sc_sdk/sc_sdk/configuration/utils/parameter_class_helper.py
@@ -3,19 +3,8 @@ This module implements utility functions related configurable parameter containe
 classes in the SC SDK
 """
 
-# INTEL CONFIDENTIAL
-#
-# Copyright (C) 2021 Intel Corporation
-#
-# This software and the related documents are Intel copyrighted materials, and
-# your use of them is governed by the express license under which they were provided to
-# you ("License"). Unless the License provides otherwise, you may not use, modify, copy,
-# publish, distribute, disclose or transmit this software or the related documents
-# without Intel's prior written permission.
-#
-# This software and the related documents are provided as is,
-# with no express or implied warranties, other than those that are expressly stated
-# in the License.
+# Copyright (C) 2022-2025 Intel Corporation
+# LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
 from sc_sdk.configuration.enums import ConfigurableParameterType
 from sc_sdk.configuration.interfaces.configurable_parameters_interface import IConfigurableParameterContainer
