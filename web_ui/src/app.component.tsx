852a22d264d28edd050feba3ccb2d65011b4e284
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-04-22 08:23:05 +0100

    CVS-165544 build: Split build into app and admin (#18)

--- web_ui/src/app.component.tsx
+++ web_ui/src/app.component.tsx
@@ -1,37 +1,10 @@
 // Copyright (C) 2022-2025 Intel Corporation
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
-import { useMemo } from 'react';
-
-import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-
-import { useFeatureFlags } from './core/feature-flags/hooks/use-feature-flags.hook';
+import { InfrastructureProviders } from './providers/infrastructure-providers.component';
 import { appRoutes } from './routes/app-routes.component';
 import { intelAdminRoutes } from './routes/intel-admin-routes';
-import { IntelBrandedLoading } from './shared/components/loading/intel-branded-loading.component';
-import { ThemeProvider } from './theme/theme-provider.component';
 
-export const IS_ADMIN_BUILD =
-    process.env.REACT_APP_BUILD_TARGET === 'admin' || process.env.REACT_APP_BUILD_TARGET === 'admin_standalone';
-
-export const App = (): JSX.Element => {
-    // Load feature flags at the top of the tree leveraging the suspense.
-    useFeatureFlags();
-
-    const router = useMemo(() => {
-        const routes = process.env.REACT_APP_INCLUDE_ALL_ROUTES
-            ? [appRoutes(), intelAdminRoutes()]
-            : IS_ADMIN_BUILD
-              ? [intelAdminRoutes()]
-              : [appRoutes()];
-
-        return createBrowserRouter(createRoutesFromElements(routes));
-    }, []);
-
-    return (
-        <ThemeProvider router={router}>
-            <RouterProvider router={router} fallbackElement={<IntelBrandedLoading />} />
-            <div id='custom-notification'></div>
-        </ThemeProvider>
-    );
+export const App = () => {
+    return <InfrastructureProviders routes={[appRoutes, intelAdminRoutes]} />;
 };
--- web_ui/src/app.test.tsx
+++ web_ui/src/app.test.tsx
@@ -1,18 +1,9 @@
 // Copyright (C) 2022-2025 Intel Corporation
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
-import { Suspense } from 'react';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 
 import { App } from './app.component';
-import { ApplicationServicesProvider } from './core/services/application-services-provider.component';
-import { NotificationProvider, Notifications } from './notification/notification.component';
-import { ProjectProvider } from './pages/project-details/providers/project-provider/project-provider.component';
-import { IntelBrandedLoading } from './shared/components/loading/intel-branded-loading.component';
-import { getMockedProjectIdentifier } from './test-utils/mocked-items-factory/mocked-identifiers';
-import { ThemeProvider } from './theme/theme-provider.component';
 
 jest.mock('react-oidc-context', () => ({
     ...jest.requireActual('react-oidc-context'),
@@ -30,26 +21,7 @@ jest.mock('react-oidc-context', () => ({
 
 describe('App component', () => {
     it('renders correctly', async () => {
-        const queryClient = new QueryClient();
-
-        render(
-            <Suspense fallback={<div>Test</div>}>
-                <QueryClientProvider client={queryClient}>
-                    <ThemeProvider>
-                        <NotificationProvider>
-                            <Suspense fallback={<IntelBrandedLoading />}>
-                                <ApplicationServicesProvider useInMemoryEnvironment>
-                                    <ProjectProvider projectIdentifier={getMockedProjectIdentifier({})}>
-                                        <App />
-                                    </ProjectProvider>
-                                </ApplicationServicesProvider>
-                            </Suspense>
-                            <Notifications />
-                        </NotificationProvider>
-                    </ThemeProvider>
-                </QueryClientProvider>
-            </Suspense>
-        );
+        render(<App />);
 
         await waitForElementToBeRemoved(screen.getByRole('progressbar'));
 
