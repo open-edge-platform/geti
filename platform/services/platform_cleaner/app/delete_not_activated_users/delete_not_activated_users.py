829c145d973c7c5addf089a0f61c9099fd1b55bc
Mariusz Gumowski <mariusz.gumowski@intel.com>
2025-04-25 20:21:54 +0200

    delete not activated tests

--- platform/services/platform_cleaner/app/delete_not_activated_users/delete_not_activated_users.py
+++ platform/services/platform_cleaner/app/delete_not_activated_users/delete_not_activated_users.py
@@ -9,9 +9,9 @@ from datetime import datetime
 from geti_logger_tools.logger_config import initialize_logger
 from grpc_interfaces.account_service.client import AccountServiceClient
 from grpc_interfaces.account_service.enums import OrganizationStatus, UserStatus
-from grpc_interfaces.account_service.protobuf.organization_status_pb2 import OrganizationStatusRequest
-from grpc_interfaces.account_service.protobuf.user_pb2 import FindUserRequest, UserIdRequest
-from grpc_interfaces.account_service.protobuf.user_status_pb2 import UserStatusRequest
+from grpc_interfaces.account_service.pb.organization_status_pb2 import OrganizationStatusRequest
+from grpc_interfaces.account_service.pb.user_pb2 import FindUserRequest, UserIdRequest
+from grpc_interfaces.account_service.pb.user_status_pb2 import UserStatusRequest
 
 logger = initialize_logger(__name__)
 
