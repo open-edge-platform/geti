b91ea682e96357543cbd9981124c8e5f280fb6ff
Mark Redeman <markredeman@gmail.com>
2025-05-14 08:16:04 +0200

    Render a react-router Link when adding href to a button (#203)

---
+++ web_ui/src/shared/components/button/button.test.tsx
@@ -0,0 +1,51 @@
+// Copyright (C) 2022-2025 Intel Corporation
+// LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
+
+import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter as Router, useLocation } from 'react-router-dom';
+
+import { ThemeProvider } from '../../../theme/theme-provider.component';
+import { Button } from './button.component';
+
+describe('Button', () => {
+    it('renders a button', async () => {
+        const onPress = jest.fn();
+        render(
+            <ThemeProvider>
+                <Router>
+                    <Button onPress={onPress}>Go to annotator</Button>
+                </Router>
+            </ThemeProvider>
+        );
+        const button = screen.getByRole('button');
+        expect(button).toBeInTheDocument();
+        fireEvent.click(button);
+        expect(onPress).toHaveBeenCalled();
+    });
+
+    it('renders a button as a link when a href is used', async () => {
+        const Pathname = () => {
+            const location = useLocation();
+
+            return <span aria-label='pathname'>{location.pathname}</span>;
+        };
+        const onPress = jest.fn();
+        render(
+            <ThemeProvider>
+                <Router>
+                    <Button href='/annotator' onPress={onPress}>
+                        Go to annotator
+                    </Button>
+                    <Pathname />
+                </Router>
+            </ThemeProvider>
+        );
+
+        const button = screen.getByRole('button');
+        expect(button).toBeInTheDocument();
+        expect(button).toHaveAttribute('href', '/annotator');
+        fireEvent.click(button);
+        expect(onPress).toHaveBeenCalled();
+        expect(screen.getByLabelText('pathname')).toHaveTextContent('/annotator');
+    });
+});
