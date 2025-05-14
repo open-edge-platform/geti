46d16648c77f7768fd3a5242d1f46b8aa7fade4d
Joao Vilaca <joao.vilaca@intel.com>
2025-05-14 14:57:47 +0200

    Replace with Spectrum Flex

--- web_ui/src/shared/components/configurable-parameters/config-params-placeholder/config-params-placeholder.component.tsx
+++ web_ui/src/shared/components/configurable-parameters/config-params-placeholder/config-params-placeholder.component.tsx
@@ -1,19 +1,18 @@
 // Copyright (C) 2022-2025 Intel Corporation
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
+import { Flex } from '@adobe/react-spectrum';
+
 import { Skeleton } from '../../skeleton/skeleton.component';
 
 export const ConfigParamsPlaceholder = (): JSX.Element => {
     return (
-        <div
-            style={{ display: 'flex', width: '100%', height: '100%', gap: 32 }}
-            data-testid={'config-params-placeholder-id'}
-        >
-            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', gap: 16 }}>
+        <Flex width={'100%'} height={'100%'} gap={32} data-testid={'config-params-placeholder-id'}>
+            <Flex flex={1} direction='column' gap={16}>
                 <Skeleton UNSAFE_style={{ height: 50, width: '40%' }} />
                 <Skeleton UNSAFE_style={{ height: 50, width: '40%' }} />
                 <Skeleton UNSAFE_style={{ height: 50, width: '40%' }} />
-            </div>
+            </Flex>
             <div
                 style={{
                     width: 1,
@@ -22,11 +21,11 @@ export const ConfigParamsPlaceholder = (): JSX.Element => {
                     alignSelf: 'flex-start',
                 }}
             />
-            <div style={{ flex: 2, display: 'flex', flexDirection: 'column', gap: 20 }}>
+            <Flex flex={2} direction='column' gap={20}>
                 <Skeleton UNSAFE_style={{ height: 100, width: '100%' }} />
                 <Skeleton UNSAFE_style={{ height: 100, width: '100%' }} />
                 <Skeleton UNSAFE_style={{ height: 100, width: '100%' }} />
-            </div>
-        </div>
+            </Flex>
+        </Flex>
     );
 };
