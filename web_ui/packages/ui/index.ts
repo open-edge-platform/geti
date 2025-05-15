a9023fe78cc3d97579398168ada75ac61490ffe6
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-05-15 11:58:57 +0200

    refactor: Move ColorHumb to the UI

--- web_ui/packages/ui/index.ts
+++ web_ui/packages/ui/index.ts
@@ -1,6 +1,6 @@
 // Copyright (C) 2022-2025 Intel Corporation
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
-export { Button, ActionButton } from './src/button/button.component';
-export type { ButtonProps, ActionButtonProps } from './src/button/button.component';
+export { Button, ActionButton, type ButtonProps, type ActionButtonProps } from './src/button/button.component';
 export { Checkbox } from './src/checkbox/checkbox.component';
+export { ColorThumb, type ColorThumbProps } from './src/color-thumb/color-thumb.component';
