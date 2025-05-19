33d52d9d16dee9d2c8406a1c524b28e0f2241917
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-05-19 13:10:28 +0200

    chore: Update eslint to block imports from react-spectrum and react-types

--- web_ui/eslint.config.js
+++ web_ui/eslint.config.js
@@ -23,13 +23,22 @@ export default [
             'no-restricted-imports': [
                 'error',
                 {
-                    name: '@adobe/react-spectrum',
-                    message: 'Use component from the @geti/ui folder instead.',
-                },
-                {
-                    name: '@react-types/button',
-                    importNames: ['SpectrumButtonProps', 'SpectrumActionButtonProps'],
-                    message: "Use component's type from @geti/ui folder instead.",
+                    paths: [
+                        {
+                            name: '@adobe/react-spectrum',
+                            message: 'Use component from the @geti/ui folder instead.',
+                        },
+                    ],
+                    patterns: [
+                        {
+                            group: ['@react-spectrum/*'],
+                            message: 'Use component from the @geti/ui folder instead.',
+                        },
+                        {
+                            group: ['@react-types/*'],
+                            message: 'Use type from the @geti/ui folder instead.',
+                        },
+                    ],
                 },
             ],
         },
