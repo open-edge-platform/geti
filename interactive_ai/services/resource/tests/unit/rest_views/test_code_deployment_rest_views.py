75689bb92e09f48570611dc0cfd1ed1482238294
Max Xiang <xiangxiang.ma@intel.com>
2025-05-20 09:00:58 +0200

    fix

--- interactive_ai/services/resource/tests/unit/rest_views/test_code_deployment_rest_views.py
+++ interactive_ai/services/resource/tests/unit/rest_views/test_code_deployment_rest_views.py
@@ -8,6 +8,6 @@ from communication.rest_views.code_deployment_rest_views import CodeDeploymentRe
 
 class TestCodeDeploymentRESTViews:
     def test_code_deployment_to_rest(self, fxt_code_deployment_rest, fxt_code_deployment):
-        result = CodeDeploymentRestViews.configurable_parameters_to_rest(code_deployment=fxt_code_deployment)
+        result = CodeDeploymentRestViews.to_rest(code_deployment=fxt_code_deployment)
 
         compare(result, fxt_code_deployment_rest, ignore_eq=True)
