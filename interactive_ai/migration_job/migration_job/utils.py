9c9cb4637d66f98e7719108cc1f8b8e09f02d5b0
Albert van Houten <albert.van.houten@intel.com>
2025-05-21 14:36:04 +0200

    remove authentication for mongodb service user creation job

--- interactive_ai/migration_job/migration_job/utils.py
+++ interactive_ai/migration_job/migration_job/utils.py
@@ -2,7 +2,6 @@
 # LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
 import os
-import re
 from collections.abc import Sequence
 
 from pymongo import MongoClient
@@ -18,13 +17,7 @@ def _get_connection_string() -> str:
     """Get the full URI to connect and authenticate with the MongoDB server"""
 
     def get_local_connection_string() -> str:
-        database_address = os.environ.get("DATABASE_ADDRESS", "mongodb://localhost:27017/")
-        database_username = os.environ.get("DATABASE_USERNAME", None)
-        database_password = os.environ.get("DATABASE_PASSWORD", None)
-        if database_username and database_password:
-            add_creds_regex = r"\1" + database_username + ":" + database_password + "@"
-            return re.sub(r"(mongodb://)", add_creds_regex, database_address)
-        return database_address
+        return os.environ.get("DATABASE_ADDRESS", "mongodb://localhost:27017/")
 
     def get_aws_connection_string() -> str:
         database_address = os.environ.get("DATABASE_ADDRESS", None)
