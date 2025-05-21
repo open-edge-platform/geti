cf72d95413d0c481451e06cab41db1bbc7fe31ee
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-05-21 14:20:14 +0200

    chore: Add core package in the packages with the basic config

--- web_ui/packages/config/test/jest.config.js
+++ web_ui/packages/config/test/jest.config.js
@@ -8,11 +8,7 @@ const pathUrl = fileURLToPath(import.meta.url);
 const dirname = path.dirname(pathUrl);
 
 export default {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
     setupFiles: ['react-app-polyfill/jsdom'],
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.tsx'],
-    testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
     testEnvironment: 'jsdom',
     transform: {
         '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': path.resolve(dirname, '../../../config/jest/babelTransform.js'),
@@ -25,22 +21,6 @@ export default {
     },
     moduleFileExtensions: ['web.js', 'js', 'web.ts', 'ts', 'web.tsx', 'tsx', 'json', 'web.jsx', 'jsx', 'node'],
     resetMocks: false,
-    coveragePathIgnorePatterns: [
-        'src/service-worker.ts',
-        'src/serviceWorkerRegistration.ts',
-        'src/webworkers',
-        'src/hooks/use-worker/utils.ts',
-        'src/theme',
-        'src/report-web-vitals.ts',
-        'src/app.component.tsx',
-        'src/index.tsx',
-        'src/assets',
-        'src/e2e',
-        'src/intel-admin-app',
-        'src/sign-up',
-        'dev-proxy.ts',
-        'tests',
-    ],
     coverageReporters: ['clover', 'json', 'json-summary'],
     coverageThreshold: {
         global: {
--- web_ui/packages/core/jest.config.js
+++ web_ui/packages/core/jest.config.js
@@ -7,6 +7,5 @@ export default {
     ...sharedConfig,
     roots: ['<rootDir>/src'],
     collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.tsx'],
     testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
 };
