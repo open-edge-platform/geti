402ca72d0e004b76a2147a05f8e6d994ae5131e6
Romanov, Evgeny <evgeny.romanov@intel.com>
2025-05-26 16:03:30 +0200

    ITEP-32475: Remove media preprocessing migration backward compatibility code

--- interactive_ai/libs/iai_core_py/iai_core/repos/mappers/mongodb_mappers/media_mapper.py
+++ interactive_ai/libs/iai_core_py/iai_core/repos/mappers/mongodb_mappers/media_mapper.py
@@ -171,9 +171,6 @@ class ImageToMongo(
             height=int(instance["height"]),
             size=int(instance["size"]),
             preprocessing=MediaPreprocessingToMongo.backward(instance["preprocessing"])
-            # TODO: remove in scope of CVS-163688
-            if "preprocessing" in instance
-            else MediaPreprocessing(status=MediaPreprocessingStatus.FINISHED),
         )
 
 
@@ -227,7 +224,4 @@ class VideoToMongo(IMapperForward[Video, dict], IMapperBackward[Video, dict]):
             total_frames=instance.get("total_frames"),  # type: ignore
             size=int(instance["size"]),
             preprocessing=MediaPreprocessingToMongo.backward(instance["preprocessing"])
-            # TODO: remove in scope of CVS-163688
-            if "preprocessing" in instance
-            else MediaPreprocessing(status=MediaPreprocessingStatus.FINISHED),
         )
