0409b01ee013a54f335d840981eda5644d3b4cdd
Romanowska, Katarzyna <katarzyna.romanowska@intel.com>
2025-05-27 08:55:03 +0200

    remove unused import

--- web_ui/tests/features/project-dataset/dataset-statistics.spec.ts
+++ web_ui/tests/features/project-dataset/dataset-statistics.spec.ts
@@ -1,7 +1,7 @@
 // Copyright (C) 2022-2025 Intel Corporation
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
-import { expect, Page } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 import { test } from '../../fixtures/base-test';
 
