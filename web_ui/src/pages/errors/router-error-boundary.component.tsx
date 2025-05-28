50692def305a2f0633cd11391bbc6c857c146ae4
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-05-28 15:19:34 +0200

    chore: Move get error message and status code to common error fallback for error boundary and router error boundary

--- web_ui/src/pages/errors/router-error-boundary.component.tsx
+++ web_ui/src/pages/errors/router-error-boundary.component.tsx
@@ -3,39 +3,12 @@
 
 import { API_URLS } from '@geti/core';
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
-import { AxiosError, isAxiosError } from 'axios';
+import { isAxiosError } from 'axios';
 import { StatusCodes } from 'http-status-codes';
-import { isRouteErrorResponse, useNavigate, useRouteError } from 'react-router-dom';
+import { useNavigate, useRouteError } from 'react-router-dom';
 
 import { ErrorFallback } from './error-boundary.component';
 import { InvalidOrganizationsScreen } from './invalid-organization/invalid-organization-screen.component';
-
-const getErrorType = (error: unknown): string => {
-    if (isRouteErrorResponse(error)) {
-        return error.data.error?.message || error.statusText;
-    }
-
-    if (isAxiosError(error)) {
-        const contentType = error.response?.headers?.['content-type'];
-        const responseURL = error.request?.responseURL;
-
-        if (contentType?.includes('text/html') && responseURL?.includes('/dex/auth/')) {
-            return StatusCodes.SERVICE_UNAVAILABLE.toString();
-        }
-
-        return error.response?.status.toString() ?? error.message;
-    }
-
-    if ('message' in (error as AxiosError)) {
-        return (error as AxiosError).message;
-    }
-
-    if (typeof error === 'string') {
-        return error;
-    }
-
-    return 'Unknown error';
-};
 
 export const RouterErrorBoundary = () => {
     const error = useRouteError();
@@ -56,9 +29,5 @@ export const RouterErrorBoundary = () => {
         return <InvalidOrganizationsScreen />;
     }
 
-    const errorType = getErrorType(error);
-
-    return (
-        <ErrorFallback error={{ name: 'router-error', message: errorType }} resetErrorBoundary={resetErrorBoundary} />
-    );
+    return <ErrorFallback error={error as Error} resetErrorBoundary={resetErrorBoundary} />;
 };
