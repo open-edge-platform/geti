858f5ca3c6c85505186d4ce731d69250ce00113e
Tybulewicz, Tomasz <tomasz.tybulewicz@intel.com>
2025-05-29 15:49:09 +0200

    ITEP-36150 Redundant call to setErrorResponse removed

--- platform/services/auth_proxy/app/request_processor/common.go
+++ platform/services/auth_proxy/app/request_processor/common.go
@@ -68,7 +68,6 @@ func (h *RequestHandler) getUserDataFromCacheOrService(subject, jwtExternalStrin
 	h.Logger.Debugf("Cache MISS for UserData related to JWT %q", jwtExternalString)
 	currentUser, err := h.dispatchGetUserRequest(subject)
 	if err != nil {
-		h.setErrorResponse(v32.StatusCode_InternalServerError, "Internal server error")
 		return nil, false, fmt.Errorf("failed to get user information (external ID %s) from the account service: %v", subject, err)
 	}
 	return currentUser, false, nil
