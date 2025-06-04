110bd6a48fd16591d361756edb1f7624ee5d807e
Ma, Xiangxiang <xiangxiang.ma@intel.com>
2025-06-04 11:24:10 +0200

    fix rest view

--- interactive_ai/services/director/app/communication/views/training_configuration_rest_views.py
+++ interactive_ai/services/director/app/communication/views/training_configuration_rest_views.py
@@ -7,7 +7,7 @@ from geti_configuration_tools.hyperparameters import Hyperparameters
 from geti_configuration_tools.training_configuration import (
     GlobalParameters,
     NullTrainingConfiguration,
-    TrainingConfiguration,
+    TrainingConfiguration, PartialTrainingConfiguration,
 )
 
 from communication.views.configurable_parameters_to_rest import ConfigurableParametersRESTViews
@@ -27,11 +27,19 @@ class TrainingConfigurationRESTViews(ConfigurableParametersRESTViews):
         cls, global_parameters: GlobalParameters, hyperparameters: Hyperparameters
     ) -> dict[str, Any]:
         # Return a combined view of global and hyperparameters for dataset preparation
-        global_parameters_rest = cls.configurable_parameters_to_rest(
-            configurable_parameters=global_parameters.dataset_preparation,
+        global_parameters_rest = (
+            cls.configurable_parameters_to_rest(
+                configurable_parameters=global_parameters.dataset_preparation,
+            )
+            if global_parameters.dataset_preparation
+            else {}
         )
-        hyperparameters_rest = cls.configurable_parameters_to_rest(
-            configurable_parameters=hyperparameters.dataset_preparation,
+        hyperparameters_rest = (
+            cls.configurable_parameters_to_rest(
+                configurable_parameters=hyperparameters.dataset_preparation,
+            )
+            if hyperparameters.dataset_preparation
+            else {}
         )
         if not isinstance(global_parameters_rest, dict) or not isinstance(hyperparameters_rest, dict):
             raise ValueError("Expected dictionary for global and hyperparameters REST views")
@@ -40,7 +48,7 @@ class TrainingConfigurationRESTViews(ConfigurableParametersRESTViews):
     @classmethod
     def training_configuration_to_rest(cls, training_configuration: TrainingConfiguration) -> dict[str, Any]:
         """
-        Get the REST view of a training configuration
+        Get the REST view of a training configuration. Also supports PartialTrainingConfiguration.
 
         :param training_configuration: training configuration
         :return: REST view of the training configuration
@@ -48,15 +56,21 @@ class TrainingConfigurationRESTViews(ConfigurableParametersRESTViews):
         if isinstance(training_configuration, NullTrainingConfiguration):
             return {}
 
+        training_params_rest = (
+            cls.configurable_parameters_to_rest(
+                configurable_parameters=training_configuration.hyperparameters.training,
+            )
+            if training_configuration.hyperparameters and training_configuration.hyperparameters.training
+            else {}
+        )
+
         rest_view = {
             "task_id": training_configuration.task_id,
             DATASET_PREPARATION: cls._dataset_preparation_to_rest(
                 global_parameters=training_configuration.global_parameters,
                 hyperparameters=training_configuration.hyperparameters,
             ),
-            TRAINING: cls.configurable_parameters_to_rest(
-                configurable_parameters=training_configuration.hyperparameters.training,
-            ),
+            TRAINING: training_params_rest,
             EVALUATION: [],  # Evaluation parameters are not yet available
         }
 
