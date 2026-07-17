f84dc69b7aa3c35f68aeda555d4e73521c870eba
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-07-17 16:40:34 +0200

    fix: License check only in tauri context

---
+++ application/ui/src/features/license/license-check.component.tsx
@@ -0,0 +1,8 @@
+// Copyright (C) 2025-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { ReactNode } from 'react';
+
+export const LicenseCheck = ({ children }: { children: ReactNode }) => {
+    return children;
+};
