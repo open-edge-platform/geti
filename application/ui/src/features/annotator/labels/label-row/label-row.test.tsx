8e708212c7389d4a0b5c883cb7d1e2771effafec
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-07-23 17:09:50 +0200

    update unit test

--- application/ui/src/features/annotator/labels/label-row/label-row.test.tsx
+++ application/ui/src/features/annotator/labels/label-row/label-row.test.tsx
@@ -108,18 +108,29 @@ describe('LabelRow', () => {
     describe('handleHotkeyChange', () => {
         const label = getMockedLabel({ id: 'label-1', name: 'old name', color: '#ffff00', hotkey: 'a' });
 
-        it('calls onUpdate on Enter when hotkey is valid', async () => {
+        it('calls onUpdate on Enter when hotkey is valid and is changed', async () => {
             const onUpdate = vi.fn();
 
             renderApp({ label, onUpdate });
 
             const hotkeyInput = screen.getByLabelText(/Hotkey input/i);
-            await userEvent.type(hotkeyInput, 'test 1');
             await userEvent.click(hotkeyInput);
-            await userEvent.keyboard('a');
+            await userEvent.keyboard('b');
             await userEvent.keyboard('{Enter}');
 
-            expect(onUpdate).toHaveBeenCalledWith(label.id, expect.objectContaining({ hotkey: 'a' }));
+            expect(onUpdate).toHaveBeenCalledWith(label.id, expect.objectContaining({ hotkey: 'b' }));
+        });
+
+        it('does not call onUpdate on Enter when hotkey is valid but is not changed', async () => {
+            const onUpdate = vi.fn();
+
+            renderApp({ label, onUpdate });
+
+            const hotkeyInput = screen.getByLabelText(/Hotkey input/i);
+            await userEvent.click(hotkeyInput);
+            await userEvent.keyboard('{Enter}');
+
+            expect(onUpdate).not.toHaveBeenCalled();
         });
 
         it('calls onUpdate when input is no longer focused', async () => {
