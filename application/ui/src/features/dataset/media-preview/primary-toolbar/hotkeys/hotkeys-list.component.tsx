4b06839f38df942194edb75cf0dfc0d2794c36ed
andersendsa <199610634+andersendsa@users.noreply.github.com>
2026-07-27 17:40:05 +0000

    revert irrelvant changes

--- application/ui/src/features/dataset/media-preview/primary-toolbar/hotkeys/hotkeys-list.component.tsx
+++ application/ui/src/features/dataset/media-preview/primary-toolbar/hotkeys/hotkeys-list.component.tsx
@@ -27,10 +27,8 @@ export const HotkeysList = () => {
 
     return (
         <Grid columns={['2fr', '1fr']} rowGap={'size-100'}>
-            <HotkeyItem
-                hotkeyName={'Submit annotations/predictions'}
-                hotkey={`${submitHotkey} or ${submitAlternativeHotkey}`}
-            />
+            <HotkeyItem hotkeyName={'Submit annotations/predictions'} hotkey={submitHotkey} />
+            <HotkeyItem hotkeyName={'Submit annotations/predictions'} hotkey={submitAlternativeHotkey} />
             <Divider size='S' gridColumn={'1/-1'} />
             <HotkeyItem hotkeyName={'Previous media'} hotkey={formatHotkeyForDisplay(HOTKEYS.previousMedia)} />
             <HotkeyItem hotkeyName={'Next media'} hotkey={formatHotkeyForDisplay(HOTKEYS.nextMedia)} />
