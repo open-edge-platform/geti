33feebcba85a692ce7e8b00d12b7170b9985b0d6
Joao Vilaca <joao.vilaca@intel.com>
2026-08-12 15:56:42 +0200

    Add skeleton layout for timm model configuration

--- application/ui/src/features/models/train-model/basic-train-model-content.component.tsx
+++ application/ui/src/features/models/train-model/basic-train-model-content.component.tsx
@@ -7,8 +7,12 @@ import { ModelArchitecturesList } from './model-architectures-list/model-archite
 import { SelectDatasetRevision } from './select-dataset-revision.component';
 import { SelectModelRevision } from './select-model-revision.component';
 import { SelectTrainingDevice } from './select-training-device/select-training-device.component';
+import { TimmModelConfiguration } from './timm-model-configuration/timm-model-configuration.component';
 
 export const BasicTrainModelContent = () => {
+    // TODO: Update this once the backend part is done
+    const isTIMMModelSelected = false;
+
     return (
         <View backgroundColor={'gray-50'} height={'100%'}>
             <Flex height={'100%'} direction={'column'} gap={'size-300'}>
@@ -17,10 +21,15 @@ export const BasicTrainModelContent = () => {
                 </View>
 
                 <Divider size={'S'} width={'100%'} />
-                <Flex gap={'size-300'} width={'100%'}>
-                    <SelectTrainingDevice />
-                    <SelectDatasetRevision />
-                    <SelectModelRevision />
+
+                <Flex direction={'column'} gap={'size-300'}>
+                    {isTIMMModelSelected && <TimmModelConfiguration />}
+
+                    <Flex gap={'size-300'} width={'100%'}>
+                        <SelectTrainingDevice />
+                        <SelectDatasetRevision />
+                        <SelectModelRevision />
+                    </Flex>
                 </Flex>
             </Flex>
         </View>
