5acc46b84ec738d1d446de1d481c22f3e07732b2
Joao Vilaca <joao.vilaca@intel.com>
2026-08-14 13:54:16 +0200

    Update more packages

--- application/ui/vitest.config.ts
+++ application/ui/vitest.config.ts
@@ -21,6 +21,9 @@ export default defineConfig({
             include: '**/*.svg',
         }),
     ],
+    resolve: {
+        tsconfigPaths: true,
+    },
     test: {
         environment: 'jsdom',
 
