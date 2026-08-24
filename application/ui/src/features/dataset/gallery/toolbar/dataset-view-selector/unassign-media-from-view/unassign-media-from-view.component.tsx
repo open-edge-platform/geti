fdeb8ff309598cc18b953370f154310a64a4434e
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-08-24 10:38:27 +0200

    remove duplicated cr header

--- application/ui/src/features/dataset/gallery/toolbar/dataset-view-selector/unassign-media-from-view/unassign-media-from-view.component.tsx
+++ application/ui/src/features/dataset/gallery/toolbar/dataset-view-selector/unassign-media-from-view/unassign-media-from-view.component.tsx
@@ -1,9 +1,6 @@
 // Copyright (C) 2025-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-// Copyright (C) 2025-2026 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-
 import { Button } from '@geti-ui/ui';
 import { useQueryClient } from '@tanstack/react-query';
 import { ENTIRE_DATASET_VIEW_ID, useDatasetViewId } from 'hooks/use-dataset-view-id.hook';
