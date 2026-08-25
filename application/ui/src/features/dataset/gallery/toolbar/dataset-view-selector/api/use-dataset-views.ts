483f7e3d99835be06df8ec2404061c6880374b32
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-08-25 09:31:56 +0200

    fix: Don't fetch dataset views because API is not ready yet

--- application/ui/src/features/dataset/gallery/toolbar/dataset-view-selector/api/use-dataset-views.ts
+++ application/ui/src/features/dataset/gallery/toolbar/dataset-view-selector/api/use-dataset-views.ts
@@ -1,22 +1,25 @@
 // Copyright (C) 2025-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-import { $api } from '@/api';
-import { useProjectIdentifier } from 'hooks/use-project-identifier.hook';
+import { DatasetView } from '../type';
 
-/*const DATASET_VIEWS: DatasetView[] = [
-    { id: 'collection-one', name: 'Collection One', created_at: '', project_id: '' },
-    { id: 'collection-two', name: 'Collection Two', created_at: '', project_id: '' },
-];*/
+const DATASET_VIEWS: DatasetView[] = [
+    { id: 'collection-one', name: 'Collection One' },
+    { id: 'collection-two', name: 'Collection Two' },
+];
 
 export const useDatasetViewsQuery = () => {
-    const projectId = useProjectIdentifier();
+    return {
+        data: DATASET_VIEWS,
+    };
 
+    /*
+    const projectId = useProjectIdentifier();
     return $api.useSuspenseQuery('get', '/api/projects/{project_id}/dataset/views', {
         params: {
             path: {
                 project_id: projectId,
             },
         },
-    });
+    });*/
 };
