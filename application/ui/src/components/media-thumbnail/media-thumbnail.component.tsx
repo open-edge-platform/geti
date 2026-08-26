37cd6a88070855853927cebeec3f565e40b362de
Joao Vilaca <joao.vilaca@intel.com>
2026-08-26 13:30:51 +0200

    Request thumbnails only when scroll stops

--- application/ui/src/components/media-thumbnail/media-thumbnail.component.tsx
+++ application/ui/src/components/media-thumbnail/media-thumbnail.component.tsx
@@ -6,6 +6,7 @@ import { useEffect, useRef } from 'react';
 import type { Media, MediaVideo } from '@/api/types';
 import { Flex } from '@geti-ui/ui';
 
+import { useIsScrolling } from '../../hooks/use-is-scrolling.hook';
 import { isVideo } from '../../shared/media-item-utils';
 import { formatCompactDuration } from './util';
 
@@ -40,24 +41,31 @@ const VideoIndicator = ({ duration }: VideoIndicatorProps) => {
 
 export const MediaThumbnail = ({ onDoubleClick, onClick, url, alt, item }: MediaThumbnailProps) => {
     const imgRef = useRef<HTMLImageElement>(null);
+    const isScrolling = useIsScrolling();
+
+    // Tiles that fly past during a fast scroll would otherwise start a request and immediately
+    // cancel it; enough of those wedge the connection and leave the gallery blank.
+    useEffect(() => {
+        if (isScrolling || imgRef.current === null) {
+            return;
+        }
+
+        imgRef.current.src = url;
+    }, [url, isScrolling]);
 
     useEffect(() => {
         const ref = imgRef.current;
-
-        if (ref) {
-            ref.src = url;
-        }
 
         return () => {
             if (ref) {
                 ref.src = '';
             }
         };
-    }, [url]);
+    }, []);
 
     return (
         <div onDoubleClick={onDoubleClick} onClick={onClick} className={classes.imgContainer}>
-            <img ref={imgRef} src={url} alt={alt} className={classes.img} draggable={false} decoding={'async'} />
+            <img ref={imgRef} alt={alt} className={classes.img} draggable={false} decoding={'async'} />
             {isVideo(item) && <VideoIndicator duration={item.duration} />}
         </div>
     );
