ca427d9f60bf0ce951fd504459d58501b2806cbb
Vitalii Karniushin <vitalii.karniushin@intel.com>
2026-09-18 13:56:23 +0200

    fix: enable WAL + set busy_timeout

--- application/backend/app/db/engine.py
+++ application/backend/app/db/engine.py
@@ -31,6 +31,8 @@ def set_sqlite_pragma(dbapi_connection: Connection, _: Any) -> None:
     # https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#foreign-key-support
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.execute("PRAGMA journal_mode=WAL")  # Enable Write-Ahead Logging (WAL) mode for better concurrency
+    cursor.execute("PRAGMA busy_timeout=5000")  # Set busy timeout to 5000 milliseconds (5 sec) to handle database locks
     cursor.close()
 
 
